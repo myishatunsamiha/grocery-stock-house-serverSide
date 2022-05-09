c75bd27e760210cef9611e607b5d5dea9e652e8c
myishatunsamiha <myisha.samiha@gmail.com>
2022-05-09 12:10:04 +0600

    implemented get api for '/inventoryhom' route to display six items from the inventory on the home page.

--- index.js
+++ index.js
@@ -1,17 +1,45 @@
 const express = require('express');
 const cors = require('cors');
 // const jwt = require('jsonwebtoken');
-
-
 require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-//middleware
+// middleware
 app.use(cors());
 app.use(express.json());
 
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hjsj3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+    try {
+        await client.connect();
+        const inventoryCollection = client.db('groceryInventory').collection('inventories');
+
+        console.log('database is connceted');
+
+        app.get('/inventoryhome', async (req, res) => {
+            const query = {};
+            const cursor = inventoryCollection.find(query);
+            const maxCount = 6;
+            itemsToDisplay = await cursor.limit(maxCount).toArray();
+            res.send(itemsToDisplay);
+        })
+
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.dir);
 
 
 // basic testing api
